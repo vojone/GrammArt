3f666957f4efc0bedce81f0cda5e68d9c5302af9
vojone <vvd1@centrum.cz>
2025-01-10 23:01:05 +0100

    refactor: fixed line endings

--- utils.js
+++ utils.js
@@ -6,20 +6,20 @@
  * @returns HTMImageElement with resized image
  */
 function resizeImage(img, width, height) {
-    let result = new Image();
-    let canvas = document.createElement('canvas');
-    canvas.height = height;
-    canvas.width = width;
+  let result = new Image();
+  let canvas = document.createElement('canvas');
+  canvas.height = height;
+  canvas.width = width;
 
-    let tmpCanvas = canvas.getContext('2d');
-    if(tmpCanvas === null) {
-        throw new Error("Error while resizing an image, cannot get a context temporary canvas.");
-    }
+  let tmpCanvas = canvas.getContext('2d');
+  if(tmpCanvas === null) {
+      throw new Error("Error while resizing an image, cannot get a context temporary canvas.");
+  }
 
-    tmpCanvas.drawImage(img, 0, 0, width, height);
-    result.src = canvas.toDataURL();
+  tmpCanvas.drawImage(img, 0, 0, width, height);
+  result.src = canvas.toDataURL();
 
-    return result;
+  return result;
 }
 
 /**
@@ -30,31 +30,31 @@ function resizeImage(img, width, height) {
  * @param heigth Height of downloaded image
  */
 function downloadCanvasContent(canvasId, name, width, height) {
-    function downloadImage(image, width, height) {
-        let tmpLink = document.createElement('a');
-        let resizedImage = resizeImage(image, width, height);
-        tmpLink.href = resizedImage.src;
-        tmpLink.download = `${name}`;
-        tmpLink.click();
-    }
+  function downloadImage(image, width, height) {
+    let tmpLink = document.createElement('a');
+    let resizedImage = resizeImage(image, width, height);
+    tmpLink.href = resizedImage.src;
+    tmpLink.download = `${name}`;
+    tmpLink.click();
+  }
 
-    let canvas = document.getElementById(canvasId);
-    if(canvas === null) {
-        throw new Error(`Unable to find canvas with id: ${canvasId}`);
-    }
+  let canvas = document.getElementById(canvasId);
+  if(canvas === null) {
+    throw new Error(`Unable to find canvas with id: ${canvasId}`);
+  }
 
-    let _width;
-    let _height;
-    if(width === null || height === null) {
-        _width = canvas.width;
-        _height = canvas.height;
-    }
-    else {
-        _width = width;
-        _height = height;
-    }
+  let _width;
+  let _height;
+  if(width === null || height === null) {
+    _width = canvas.width;
+    _height = canvas.height;
+  }
+  else {
+    _width = width;
+    _height = height;
+  }
 
-    let img = new Image();
-    img.src = canvas.toDataURL();
-    img.onload = () => { downloadImage(img, _width, _height); };
+  let img = new Image();
+  img.src = canvas.toDataURL();
+  img.onload = () => { downloadImage(img, _width, _height); };
 }
