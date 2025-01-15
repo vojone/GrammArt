84fbe9cbaf0eaaa59d5b374df9b5d2087c53a0bd
vojone <vvd1@centrum.cz>
2025-01-15 21:45:41 +0100

    feat: added prototype of line numbering feature

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ const Parser = window.TreeSitter;
   await main();
 })();
 
+var dbg;
 async function main(params) {
   const parser = new Parser();
   const languagePath = "tree-sitter-grammartcfg/tree-sitter-grammartcfg.wasm";
@@ -61,5 +62,64 @@ async function main(params) {
     let editor = $("#code-editor");
     let editorContent = editor.text();
     //console.log(document.getSelection());
+  });
+
+  var origLineNumber = null;
+  function updateLineNumbering(lineNumber) {
+    $("#line-numbering").html("");
+    for (let line = 0; line <= lineNumber; line++) {
+      $("#line-numbering").append(`<div class="lineno">${line + 1}</div>`)
+    }
+
+    origLineNumber = lineNumber;
+  }
+
+  $("#code-editor")[0].addEventListener("beforeinput", (e) => {
+    const newlineRegexp = /((\r?\n)|<br>)/g;
+    var extraNewlines = 0;
+    var insertedText = "";
+
+    if(origLineNumber === null) {
+      const originalText = $("#code-editor").text();
+      const originalNewlines = originalText.matchAll(newlineRegexp).toArray();
+      extraNewlines = originalNewlines.length;
+      origLineNumber = 0;
+    }
+
+    var affectedText = $("#code-editor").html().substring(
+      e.getTargetRanges()[0].startOffset,
+      e.getTargetRanges()[0].startOffset > e.getTargetRanges()[0].endOffset ? e.getTargetRanges()[0].startOffset + e.getTargetRanges()[0].endOffset : e.getTargetRanges()[0].endOffset
+    );
+    const deletedNewlines = affectedText.matchAll(newlineRegexp).toArray();
+    extraNewlines -= deletedNewlines.length;
+
+    if(e.inputType == "insertFromPaste") {
+      e.dataTransfer.items[0].getAsString((data) => {
+        const pastedTextRegexp = /<!--StartFragment-->(.*)<!--EndFragment-->/gsi;
+        const pastedText = data.match(pastedTextRegexp);
+        const pastedNewlines = pastedText[0].matchAll(newlineRegexp).toArray();
+        extraNewlines += pastedNewlines.length;
+
+        if(extraNewlines != 0) {
+          updateLineNumbering(origLineNumber + extraNewlines);
+        }
+
+        $("#code-editor")[0].normalize();
+      });
+
+      return;
+    }
+    else if(e.inputType == "insertParagraph") {
+      insertedText = "\n";
+    }
+    else if(e.inputType == "insertText") {
+      insertedText = e.data;
+    }
+
+    const newNewlines = insertedText.matchAll(newlineRegexp).toArray();
+    extraNewlines += newNewlines.length;
+    if(extraNewlines != 0) {
+      updateLineNumbering(origLineNumber + extraNewlines);
+    }
   });
 }
