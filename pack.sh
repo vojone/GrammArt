068a7b5252c0be7de618cbbc30d4e5ce1c8db889
vojone <vvd1@centrum.cz>
2025-02-02 21:26:25 +0100

    fix: added pack.sh to pack script

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -r xdvora3o.zip README.md *.js index.html style.css tree-sitter-grammartcfg/grammar.js results images dist deps
+zip -r xdvora3o.zip README.md *.js index.html style.css tree-sitter-grammartcfg/grammar.js results images dist deps favicon.png
